47c9ce14fa056ee17ab67e620845b913fae5302d
aromarious <6535448+aromarious@users.noreply.github.com>
2025-12-16 12:25:31 +0900

    chore: remove unused User table and types (#23)
    * wip: start implementation
    
    * chore: remove unused User table and types

--- packages/db/src/schema.ts
+++ packages/db/src/schema.ts
@@ -1,16 +1,6 @@
 import { pgTable } from "drizzle-orm/pg-core"
 import { ulid } from "ulid"
 
-export const UserTable = pgTable("users", (t) => ({
-  id: t
-    .text("id")
-    .primaryKey()
-    .$defaultFn(() => ulid()),
-  name: t.text("name"),
-  email: t.text("email").notNull(),
-  createdAt: t.timestamp("created_at").defaultNow().notNull(),
-}))
-
 export const PostTable = pgTable("posts", (t) => ({
   id: t
     .text("id")
@@ -21,9 +11,6 @@ export const PostTable = pgTable("posts", (t) => ({
   createdAt: t.timestamp("created_at").defaultNow().notNull(),
   updatedAt: t.timestamp("updated_at").defaultNow().notNull(),
 }))
-
-export type User = typeof UserTable.$inferSelect
-export type NewUser = typeof UserTable.$inferInsert
 
 export type Post = typeof PostTable.$inferSelect
 export type NewPost = typeof PostTable.$inferInsert
